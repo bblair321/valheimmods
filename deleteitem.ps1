d0a7e0df9815b23c08cfc9c4ba3f02901915ceed
bblair321 <81390380+bblair321@users.noreply.github.com>
2021-04-28 10:39:03 -0700

    Added a script that you can use to delete stuff

---
+++ deleteitem.ps1
@@ -0,0 +1,9 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+ 
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+    Multiselect = $true
+}
+ 
+[void]$FileBrowser.ShowDialog()
+$FileBrowser.FileNames
+Remove-Item -Confirm $FileBrowser.FileNames
