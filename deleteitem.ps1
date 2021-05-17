fddf105ffb706932147c06fb3212e87ebfcc8336
bblair321 <81390380+bblair321@users.noreply.github.com>
2021-05-17 14:26:33 -0700

    Delete deleteitem.ps1

--- deleteitem.ps1
+++
@@ -1,9 +0,0 @@
-﻿Add-Type -AssemblyName System.Windows.Forms
- 
-$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
-    Multiselect = $true
-}
- 
-[void]$FileBrowser.ShowDialog()
-$FileBrowser.FileNames
-Remove-Item -Confirm $FileBrowser.FileNames
